e700c63a3675ced54b07c0717db26ab9a0d7c6eb
Tirasigor <31933244+Tirasigor@users.noreply.github.com>
2022-08-10 22:42:09 +0300

    Delete js directory

--- js/accordion.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.faq__accordion-trigger').forEach(function (item) {
-	item.addEventListener('click', () => {
-		console.log(1);
-		const parent = item.parentNode;
-		parent.classList.toggle('faq__accordion-item_active');
-	})
-}
-)
--- js/burger.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.header__burger');
-const burgerMenu = document.querySelector('.header__burger-menu');
-
-burger.addEventListener('click', (e) => {
-	e.preventDefault;
-	burger.classList.toggle('header__burger_active');
-	burgerMenu.classList.toggle('header__burger-menu_active');
-})
